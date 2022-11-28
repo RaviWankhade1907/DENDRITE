af9cfbbccb8b40cc6b955ccfd999456f962f71d7
raviwankhade1907 <raviwankhade786@gmail.com>
2022-11-28 16:38:04 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+// import './Home.css';
+// import Button from 'react-bootstrap/Button';
+// import { useEffect, useState } from 'react';
+// import { Link } from "react-router-dom";
+import React from 'react';
+
+function Home(props) {
+
+    
+    return (
+        <div>
+     
+        </div>
+    );
+}
+
+export default Home;
